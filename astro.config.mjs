2370a47744bf32a7956fb5215b34c5b5224d067f
Jose Olvera <8121110088@utchetumal.edu.mx>
2025-02-25 14:38:02 -0500

    cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
-
+import vercelServerless from '@astrojs/vercel/serverless';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: 'server',
+    adapter: vercelServerless(),
+});
